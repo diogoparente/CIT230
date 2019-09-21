8edb3dda7a7ba8f105cb9ae62466921f0aa69b04
diogoparente <parente.diogo@gmail.com>
2019-09-21 14:16:53 -0300

    a

--- functions/dateVerify.js
+++ functions/dateVerify.js
@@ -1,10 +1,4 @@
 function dates() {
-    var dayName = "Sunday";
-      var month = "January";
-      var dayNumber = 1;
-      var year = 2019;
-      var date1 = " ";
-      
       var date1 = new Date();
       var year = date1.getFullYear();
       document.getElementById("currentYear").innerHTML = year;
