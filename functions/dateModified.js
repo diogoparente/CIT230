90b08c275be8f65aad15116ee550caa2458e4d29
diogoparente <parente.diogo@gmail.com>
2019-09-21 12:31:25 -0300

    Update

---
+++ functions/dateModified.js
@@ -0,0 +1,10 @@
+function dateModified() {
+    var dayName = "Sunday";
+      var month = "January";
+      var dayNumber = 4;
+      var year = 2019;
+      var date1 = " ";
+      
+      date1 = dayName + ", " + month + " " + dayNumber + ", " + year;
+      document.getElementById("dateModified").innerHTML = date1;
+}
