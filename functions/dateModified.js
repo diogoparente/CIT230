fefea01f122aee4ed76d5b6af88e0928a1718699
Diogo Parente <31190447+diogoparente@users.noreply.github.com>
2019-09-21 13:55:32 -0300

    Update dateModified.js

--- functions/dateModified.js
+++ functions/dateModified.js
@@ -1,11 +1,4 @@
 function dateModified() {
-    var dayName = "Sunday";
-      var month = "January";
-      var dayNumber = 1;
-      var year = 2019;
-      var date1 = " ";
-      
-      
-      date1 = document.lastModified;
+      var date1 = document.lastModified;
       document.getElementById("dateModified").innerHTML = date1;
 }
