9a7e13ef46a5d1496ccb4ca0aba6b87696d1b4e6
diogoparente <parente.diogo@gmail.com>
2019-10-12 18:12:55 -0300

    a

--- functions/dateVerify.js
+++ functions/dateVerify.js
@@ -5,4 +5,15 @@ function dates() {
 
       date2 = document.lastModified;
       document.getElementById("dateModified").innerHTML = date2;
+
+// Current Date: dayName, Number Month Year
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var currentDate = new Date()
+var currentDay = days[currentDate.getDay()];
+var currentNumDay = currentDate.getDate();
+var currentMonth = months[currentDate.getMonth()];
+var currentYear = currentDate.getFullYear();
+document.getElementById("currentDate").innerHTML = currentDay + ", " + currentNumDay + " " + currentMonth + " " + currentYear;
 }
