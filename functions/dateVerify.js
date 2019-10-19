e0462236245bd26ada9a52532b219ba4259c7dfc
diogoparente <parente.diogo@gmail.com>
2019-10-19 20:29:51 -0300

    a

--- functions/dateVerify.js
+++ functions/dateVerify.js
@@ -1,19 +1,23 @@
 function dates() {
-      var date1 = new Date();
-      var year = date1.getFullYear();
-      document.getElementById("currentYear").innerHTML = year;
+  var date1 = new Date();
+  var year = date1.getFullYear();
+  document.getElementById("currentYear").innerHTML = year;
 
-      date2 = document.lastModified;
-      document.getElementById("dateModified").innerHTML = date2;
+  date2 = document.lastModified;
+  document.getElementById("dateModified").innerHTML = date2;
 
-// Current Date: dayName, Number Month Year
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  // Current Date: dayName, Number Month Year
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var currentDate = new Date()
-var currentDay = days[currentDate.getDay()];
-var currentNumDay = currentDate.getDate();
-var currentMonth = months[currentDate.getMonth()];
-var currentYear = currentDate.getFullYear();
-document.getElementById("currentDate").innerHTML = currentDay + ", " + currentNumDay + " " + currentMonth + " " + currentYear;
+  var currentDate = new Date()
+  var currentDay = days[currentDate.getDay()];
+  var currentNumDay = currentDate.getDate();
+  var currentMonth = months[currentDate.getMonth()];
+  var currentYear = currentDate.getFullYear();
+  document.getElementById("currentDate").innerHTML = currentDay + ", " + currentNumDay + " " + currentMonth + " " + currentYear;
+
+  //Hide or show the alert for pancakes
+  if (currentDate.getDay() == 5)
+    document.getElementById("pancake").classList.toggle("hideAlert");
 }
