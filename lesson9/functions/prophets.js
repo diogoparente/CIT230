5de8064de2df61d911c17a9526ae2757f951b74f
diogoparente <parente.diogo@gmail.com>
2019-11-12 22:46:25 -0300

    a

--- lesson9/functions/prophets.js
+++ lesson9/functions/prophets.js
@@ -5,16 +5,32 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
 
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < prophets.length; i++) {
       let card = document.createElement('section');
-      
+
       let h2 = document.createElement('h2');
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            card.appendChild(h2);
-      
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      card.appendChild(h2);
+
+      let birth = document.createElement('p');
+      birth.textContent = "Date of Birth: " + prophets[i].birthdate;
+      card.appendChild(birth);
+
+      let place = document.createElement('p');
+      place.textContent = "Place of Birth: " + prophets[i].birthplace;
+      card.appendChild(place);
+
+      let photo = document.createElement('img');
+      photo.setAttribute('src', prophets[i].imageurl);
+      photo.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' +prophets[i].order);
+      card.appendChild(photo);
+
       document.querySelector('div.cards').appendChild(card);
+    }
   });
 
-  const prophets = jsonObject['prophets'];
+
+ // <img class="image" src="images/Netflix.jpg" alt="Netflix platform"></img>
